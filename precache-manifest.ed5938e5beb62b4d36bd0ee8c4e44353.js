8a658487d71bdc2555fe9dc65587b3badbed7deb
YuraBobeshko <gorobech55@gmail.com>
2019-11-18 18:30:37 +0200

    Updates

---
+++ precache-manifest.ed5938e5beb62b4d36bd0ee8c4e44353.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840c7e9770bb29ac031859b75f858c3c",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "ce10626247110da99071",
+    "url": "/test/static/css/main.da4cda1b.chunk.css"
+  },
+  {
+    "revision": "a522bef34cf69917190b",
+    "url": "/test/static/js/2.0895d705.chunk.js"
+  },
+  {
+    "revision": "ce10626247110da99071",
+    "url": "/test/static/js/main.917c4719.chunk.js"
+  },
+  {
+    "revision": "6644ee626515b744a727",
+    "url": "/test/static/js/runtime-main.fa8c1994.js"
+  },
+  {
+    "revision": "f77c0109d8393bd5174712484dd1a5f3",
+    "url": "/test/static/media/BG__IMG.f77c0109.jpg"
+  }
+]);
